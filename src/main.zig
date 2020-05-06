4b8a820c51f9e8af8c50a8dbc5ef33d30f76e44c
pfg <omegariisthebest@gmail.com>
2020-05-06 15:58:31 -0700

    demo text

--- src/main.zig
+++ src/main.zig
@@ -21,6 +21,8 @@ pub fn main() !void {
     const screenWidth = 800;
     const screenHeight = 450;
 
+    const demotxt = "pub fn main() !void {\n    @import(\"std\").debug.warn(\"Hello, {}!\", .{\"World\"})\n}";
+
     c.SetConfigFlags(c.FLAG_WINDOW_RESIZABLE);
     c.InitWindow(screenWidth, screenHeight, "raylib demo");
     defer c.CloseWindow();
@@ -38,7 +40,7 @@ pub fn main() !void {
             c.ClearBackground(.{ .r = 46, .g = 52, .b = 64, .a = 255 });
             var x: c_int = 10;
             var y: c_int = 10;
-            for ("Hello, World!") |char| {
+            for (demotxt) |char| {
                 if (char == '\n') {
                     x = 10;
                     y += 11;
