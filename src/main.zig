186d7d3af935842bbaf7c4c6262bffd34b437746
pfg <omegariisthebest@gmail.com>
2020-05-06 16:57:52 -0700

    colors

--- src/main.zig
+++ src/main.zig
@@ -31,14 +31,57 @@ const Style = struct {
     bump: ?c.Color = null,
 };
 
+const HLStyle = union(enum) {
+    keyword: void,
+    variable: void,
+    uservar: void,
+    string: void,
+    control: void,
+    spacing: void,
+    typ: void,
+};
+
 pub fn main() !void {
     const screenWidth = 800;
     const screenHeight = 450;
 
-    const demotxt = "const std = @import(\"std\");\n" ++
-        "pub fn main() !void {\n" ++
-        "\tstd.debug.warn(\"Hi!\");\n" ++
-        "}\n";
+    const demodata = [_]struct { text: []const u8, styl: HLStyle }{
+        .{ .text = "const", .styl = .keyword },
+        .{ .text = " ", .styl = .spacing },
+        .{ .text = "std", .styl = .uservar },
+        .{ .text = " ", .styl = .spacing },
+        .{ .text = "=", .styl = .control },
+        .{ .text = " ", .styl = .spacing },
+        .{ .text = "@", .styl = .control },
+        .{ .text = "import", .styl = .keyword },
+        .{ .text = "(\"", .styl = .control },
+        .{ .text = "std", .styl = .string },
+        .{ .text = "\");", .styl = .control },
+        .{ .text = "\n", .styl = .spacing },
+        .{ .text = "pub", .styl = .keyword },
+        .{ .text = " ", .styl = .spacing },
+        .{ .text = "fn", .styl = .keyword },
+        .{ .text = " ", .styl = .spacing },
+        .{ .text = "main", .styl = .uservar },
+        .{ .text = "()", .styl = .control },
+        .{ .text = " ", .styl = .spacing },
+        .{ .text = "!", .styl = .control },
+        .{ .text = "void", .styl = .typ },
+        .{ .text = " ", .styl = .spacing },
+        .{ .text = "{", .styl = .control },
+        .{ .text = "\n\t", .styl = .spacing },
+        .{ .text = "std", .styl = .variable },
+        .{ .text = ".", .styl = .control },
+        .{ .text = "debug", .styl = .variable },
+        .{ .text = ".", .styl = .control },
+        .{ .text = "warn", .styl = .uservar },
+        .{ .text = "(\"", .styl = .control },
+        .{ .text = "Hi!", .styl = .string },
+        .{ .text = "\");", .styl = .control },
+        .{ .text = "\n", .styl = .spacing },
+        .{ .text = "}", .styl = .control },
+        .{ .text = "\n", .styl = .spacing },
+    };
 
     c.SetConfigFlags(c.FLAG_WINDOW_RESIZABLE);
     c.InitWindow(screenWidth, screenHeight, "raylib demo");
@@ -64,28 +107,32 @@ pub fn main() !void {
         }
         if (camera.zoom < 1) camera.zoom = 1;
 
-        {
-            c.BeginDrawing();
-            defer c.EndDrawing();
+        c.BeginDrawing();
+        defer c.EndDrawing();
 
-            c.ClearBackground(hex(0x222034));
+        c.ClearBackground(hex(0x222034));
 
-            c.BeginMode2D(camera);
-            defer c.EndMode2D();
+        c.BeginMode2D(camera);
+        defer c.EndMode2D();
 
-            var x: c_int = 0;
-            var y: c_int = 0;
-            var lineno: c_int = 1;
-            const left: c_int = 50;
-            const top: c_int = 10;
-            for (demotxt) |char| {
+        var x: c_int = 0;
+        var y: c_int = 0;
+        var lineno: c_int = 1;
+        const left: c_int = 50;
+        const top: c_int = 10;
+        for (demodata) |demodat| {
+            for (demodat.text) |char| {
                 if (char == '\t') {
                     x += 1;
                 }
-                const style: Style = switch (char) {
-                    '\t', ' ', '\n' => .{ .color = hex(0x313049) },
-                    '(', ')', '{', '}', ';', '"' => .{ .color = hex(0x5b597e) },
-                    else => .{ .color = hex(0xFFFFFF), .bump = hex(0x65458f) },
+                const style: Style = switch (demodat.styl) {
+                    .spacing => .{ .color = hex(0x313049) },
+                    .control => .{ .color = hex(0x5b597e) },
+                    .variable => .{ .color = hex(0xFFFFFF) },
+                    .uservar => .{ .color = hex(0xFFFFFF), .bump = hex(0x65458f) },
+                    .keyword => .{ .color = hex(0x37946e) },
+                    .typ => .{ .color = hex(0xdf7126) },
+                    .string => .{ .color = hex(0x6abe30) },
                 };
                 if (style.bump) |bump|
                     renderChar(texture, char, bump, x + left, y + top + 1);
